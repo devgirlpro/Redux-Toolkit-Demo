7c09212d41e4d2ff091fc6c5bf955c4c7f134bee
devgirlpro <agalidari@gmail.com>
2024-02-29 23:59:35 +0100

    added icecrea,Slice , import icereameReducer to the storeattached it to the store and impoer actions to the inde.js and dispatch a few of them

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const store = require('./app/store');
 const cakeActions = require('./features/cake/cakeSlice').cakeActions;
+const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions;
 
 console.log('initial state =>', store.getState());
 
@@ -7,8 +8,17 @@ const unsubscribe = store.subscribe(() => {
     console.log('updated State =>', store.getState())
 })
 
+
+store.dispatch(icecreamActions.ordered())
+
  store.dispatch(cakeActions.ordered())
  store.dispatch(cakeActions.ordered())
  store.dispatch(cakeActions.ordered())
+
+ store.dispatch(icecreamActions.ordered())
+ store.dispatch(icecreamActions.ordered())
+ store.dispatch(icecreamActions.restocked(4))
+
+
 
  store.dispatch(cakeActions.restocked(5))
