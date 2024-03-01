0a6ef2f21cca18834d563aff501d65f4a823bbec
devgirlpro <agalidari@gmail.com>
2024-03-01 15:09:10 +0100

    Async thunk for fettching Api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const store = require('./app/store');
 const cakeActions = require('./features/cake/cakeSlice').cakeActions;
 const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions;
+const fetchUsers = require('./features/user/userSlice').fetchUsers;
 
 console.log('initial state =>', store.getState());
 
@@ -9,20 +10,18 @@ const unsubscribe = store.subscribe(() => {
     // console.log('updated State =>', store.getState())
 })
 
+ store.dispatch(fetchUsers())
+
+//  store.dispatch(cakeActions.ordered())
+//  store.dispatch(cakeActions.ordered())
+//  store.dispatch(cakeActions.ordered())
+//  store.dispatch(cakeActions.restocked(8))
 
 
- store.dispatch(cakeActions.ordered())
- store.dispatch(cakeActions.ordered())
- store.dispatch(cakeActions.ordered())
- store.dispatch(cakeActions.restocked(8))
-
-
- store.dispatch(icecreamActions.ordered())
- store.dispatch(icecreamActions.ordered())
- store.dispatch(icecreamActions.restocked(5))
+//  store.dispatch(icecreamActions.ordered())
+//  store.dispatch(icecreamActions.ordered())
+//  store.dispatch(icecreamActions.restocked(5))
 
 
 
- store.dispatch(cakeActions.restocked(5))
-
- unsubscribe()
+ //unsubscribe()
